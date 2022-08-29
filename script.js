1fef5443baa7588d61ae638a1d3e52583304019d
ppeyliang <ppeyliang@gmail.com>
2022-08-29 10:55:25 +0800

    implemented sticky header

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const headerEl = document.querySelector('.header');
+const heroEl = document.querySelector('.hero');
+
+const headerHeight = headerEl.getBoundingClientRect().height;
+
+const sticky = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) {
+    headerEl.classList.add('header-sticky');
+  } else {
+    headerEl.classList.remove('header-sticky');
+  }
+};
+
+const heroObserver = new IntersectionObserver(sticky, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+});
+
+heroObserver.observe(heroEl);
